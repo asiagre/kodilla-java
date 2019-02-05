3443f47d9958a6f31c519c3ab11042a66fa76006
Joanna Zawiślak <asia.greczka@gmail.com>
2019-02-05 20:52:14 +0100

    Exercise_6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,43 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        int resultOfAdding = calculator.add(4, 5);
+
+        int resultOfSubtraction = calculator.subtract(6, 3);
+
+        System.out.print("Adding testing: ");
+
+        if(resultOfAdding == (4 + 5)) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.print("Subtraction testing: ");
+
+        if(resultOfSubtraction == (6 - 3)) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
