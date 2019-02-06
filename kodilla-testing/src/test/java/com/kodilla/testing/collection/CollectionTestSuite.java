db7ce9e326e7f2f3eaa15e612bf0dd4a12518195
Joanna Zawiślak <asia.greczka@gmail.com>
2019-02-06 21:30:49 +0100

    Exercise_6.3v2

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -13,23 +13,35 @@ public class CollectionTestSuite {
     @Test
     public void testOddNumbersExterminatorEmptyList() {
         System.out.println("Testing empty list.");
+        //given
         List<Integer> emptyArrayList = new ArrayList<Integer>();
+        //when
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         List<Integer> result = oddNumbersExterminator.exterminate(emptyArrayList);
+        //then
         Assert.assertEquals(emptyArrayList, result);
     }
 
     @Test
     public void testOddNumbersExterminatorNormalList() {
         System.out.println("Testing normal list.");
+        //given
         List<Integer> normalList = new ArrayList<Integer>();
-        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         for(int i = 0; i < 10; i++) {
             normalList.add(i);
         }
+        //when
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
         List<Integer> result = oddNumbersExterminator.exterminate(normalList);
-        for(Integer number : result) {
-            Assert.assertTrue(number % 2 == 0);
+        List<Integer> testList = new ArrayList<Integer>();
+        for(int i = 0; i < normalList.size(); i++) {
+            if(normalList.get(i) % 2 == 0) {
+                testList.add(normalList.get(i));
+            }
+        }
+        //then
+        for(int i = 0; i < result.size(); i++) {
+            Assert.assertEquals(result.get(i), testList.get(i));
         }
     }
 
