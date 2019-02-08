276e18f0914c1044584ca3abd9b581a535a11696
Joanna Zawiślak <asia.greczka@gmail.com>
2019-02-08 19:03:39 +0100

    Exercise_6.6

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -7,22 +7,30 @@ public class ShapeCollector {
     List<Shape> figures = new ArrayList<Shape>();
 
     public void addFigure(Shape shape) {
-
+        figures.add(shape);
     }
 
     public boolean removeFigure(Shape shape) {
+        if(figures.size() == 0) return false;
+        else {
+            figures.remove(shape);
+        }
         return true;
     }
 
     public Shape getFigure(int n) {
-        return null;
+        return figures.get(n);
     }
 
     public String showFigures() {
-        return null;
+        String listOfFigures = "";
+        for(Shape figure : figures) {
+            listOfFigures += figure.toString() + "\n";
+        }
+        return listOfFigures;
     }
 
     public int getFiguresQuantity() {
-        return 100;
+        return figures.size();
     }
 }
