fd8aaaea4482bf0af05bc65962bf2d38206cff7c
Joanna Zawiślak <asia.greczka@gmail.com>
2019-02-20 13:02:33 +0100

    Exercise_8.3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,15 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(2.0, 4.0);
+        } catch(Exception e) {
+            System.out.println("Something went wrong. Error:" + e);
+        }
+    }
+}
