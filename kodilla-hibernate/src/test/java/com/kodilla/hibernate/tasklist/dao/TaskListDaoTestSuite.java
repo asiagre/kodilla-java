d430e7c725d7e14028e445f4f2fc879d611f6f08
Joanna Zawiślak <asia.greczka@gmail.com>
2019-05-06 16:23:04 +0200

    Exercise_17.2

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,36 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskListDaoTestSuite {
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    public void testFindByListName() {
+        //When
+        TaskList taskList = new TaskList("ToDo", "Tasks to do");
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List<TaskList> taskLists = taskListDao.findByListName(listName);
+
+        //Then
+        Assert.assertEquals(1, taskLists.size());
+
+        //CleanUp
+        int id = taskLists.get(0).getId();
+        taskListDao.deleteById(id);
+    }
+}
